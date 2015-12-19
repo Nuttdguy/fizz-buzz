465fcb937d91f534d6b8b541ce47a6c5e04a1bdd
Nuttdguy <pygnasak@gmail.com>
2015-12-18 20:34:43 -0600

    complete fizz-buzz

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
-//Fizz-Buzz
-//Objective: Print out each number from 1 to 100.
-//For every number divisable by 3, replace with the word fizz
-//For every number divisable by 5, replace with the word buzz
-//For every number divisable by both 3 & 5, replace with fizz buzz
-
-//1. Check the value of count
-//2. Modulo the count by the current-count
-//3. Use control flow to determine output
-
 $(document).ready(function() {
-
-	for (count = 1; count <= 100;) {
-		if (count % 3 === 0) {
-			if (count % 5 === 0) {
-				$('.number-container').append('<h1>Fizz-Buzz</h1>');
-			} else {
-				$('.number-container').append('<h1>Fizz</h1>');
-			} 
-		} else if (count % 5 === 0) {
-			$('.number-container').append('<h1>Buzz</h1>');
-		} else {
-			$('.number-container').append('<h1>' + count + '</h1>');
-		}
-		
-	console.log(count)
-	count++;
-	}
+	
+	fizzBuzz();
+	resetFizzBuzz();
+	
 });
 
+var resetFizzBuzz = function() {
+	
+	// for resetting the screen
+	$(this).on('click', function() {
+		$('.main-section h1').remove();
+	});
+	
+}
 
-
+var fizzBuzz = function() {
+	
+	$(this).keypress(function(k) {
+		var keyPress = k.which;
+		console.log(keyPress); // capture the current keypress event
+		
+		var inputBoxValue; // variable for storing users inputted value
+		if (keyPress === 13) {
+			
+			function resetInput() {
+				$('.enter-number').val('');
+			} // named function that resets the number input box
+			
+			var inputValue = $('.enter-number').val();
+			if (!$.isNumeric(inputValue)) {
+				resetInput(); // could I just put return here?? End the program
+			} else {
+				inputBoxValue = parseInt($('.enter-number').val());
+				// assigning the users input value. Returns to global variable/scope.
+			}
+			resetInput(); // resets the input box after value has been stored.
+			
+		}	
+		
+		// the program for fizz-buzz
+		function runFizzBuzz(userInput) {
+			for (count = userInput; count <= 100;) {
+				if (count % 3 === 0) {
+					if (count % 5 === 0) {
+						$('.number-container').append('<h1>Fizz-Buzz</h1>');
+					} else {
+						$('.number-container').append('<h1>Fizz</h1>');
+					}
+				} else if (count % 5 === 0) {
+					$('.number-container').append('<h1>Buzz</h1>');
+				} else {
+					$('.number-container').append('<h1>' + count + '</h1>');
+				}
+			count++;
+			}
+		}
+		// instantiate the function and passing the users input value
+		runFizzBuzz(inputBoxValue);	
+	})
+};
 
 
 
